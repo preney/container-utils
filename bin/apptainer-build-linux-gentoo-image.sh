d420b58ec3111f7ac552e06cabea1f000eca918c
Paul Preney <paul@preney>
2024-06-20 21:54:50 -0400

    Updated Gentoo script

--- bin/apptainer-build-linux-gentoo-image.sh
+++ bin/apptainer-build-linux-gentoo-image.sh
@@ -2,10 +2,8 @@
 
 ###############################################################################
 ###############################################################################
-# Copyright (c) 2023 Paul Preney.
+# Copyright (c) 20232-2024 Paul Preney.
 # Distributed under the terms of the GNU Public License version 3
-#
-# NOTE: See bottom of this script for commands to build images.
 #
 # This script builds Gentoo Apptainer SIF images using Boostrap: scratch 
 # by downloading a stage3 tarball as a starting point.
@@ -36,55 +34,56 @@ Bootstrap: localimage
 From: $GENTOO_ROOTFS
 
 %post
-  # Run emerge-webrsync...
-  source /etc/profile
-  emerge-webrsync
+    # Run emerge-webrsync...
+    source /etc/profile
+    emerge-webrsync
 
-  # Enable the first non-deprecated profile...
-  #   * determine all deprecated profiles
-  TMPFILE1=\`mktemp\`
-  find /var/db/repos/gentoo/profiles/default \
-    -mindepth 3 -type f -name 'deprecated' | \
-    cut -d/ -f7- | sed 's|/deprecated$||' | sort -u >\$TMPFILE1
+    # Enable the first non-deprecated profile...
+    #   * determine all deprecated profiles
+    TMPFILE1=\`mktemp\`
+    find /var/db/repos/gentoo/profiles/default \
+        -mindepth 3 -type f -name 'deprecated' | \
+        cut -d/ -f7- | sed 's|/deprecated$||' | sort -u >\$TMPFILE1
 
-  #   * determine all profiles
-  TMPFILE2=\`mktemp\`
-  find /var/db/repos/gentoo/profiles/default \
-    -mindepth 3 -type d | cut -d/ -f7- | sort -u >\$TMPFILE2
+    #   * determine all profiles
+    TMPFILE2=\`mktemp\`
+        find /var/db/repos/gentoo/profiles/default \
+        -mindepth 3 -type d | cut -d/ -f7- | sort -u >\$TMPFILE2
 
-  #   * compute the set difference
-  TMPFILE3=\`mktemp\`
-  comm -13 \$TMPFILE1 \$TMPFILE2 >\$TMPFILE3
+    #   * compute the set difference
+    TMPFILE3=\`mktemp\`
+        comm -13 \$TMPFILE1 \$TMPFILE2 >\$TMPFILE3
 
-  #   * remove prefix, systemd, and no-multilib entries,
-  #     sort by string length, strip out architecture, and
-  #     output first entry
-  TMPFILE4=\`mktemp\`
-  cat \$TMPFILE3 | grep -v '/prefix' | grep -v '/systemd' | \
+    #   * remove prefix, systemd, and no-multilib entries,
+    #     sort by string length, strip out architecture, and
+    #     output first entry
+    TMPFILE4=\`mktemp\`
+    cat \$TMPFILE3 | grep -v '/prefix' | grep -v '/systemd' | \
         grep -v '/no-multilib' | \
         awk '{ print length, \$0 }' | sort -n | cut -d' ' -f2- \
         | grep amd64 | head -n 1 >\$TMPFILE4
 
-  PROFILE=\`cat \$TMPFILE4\`
-  echo "Selecting profile: \$PROFILE"
-  eselect profile set \$PROFILE
-  unset PROFILE
-  rm -f TMPFILE1 TMPFILE2 TMPFILE3 TMPFILE4
+    PROFILE=\`cat \$TMPFILE4\`
+    echo "Selecting profile: \$PROFILE"
+    eselect profile set \$PROFILE
+    unset PROFILE
+    rm -f TMPFILE1 TMPFILE2 TMPFILE3 TMPFILE4
 
-  # Either enable all locales or the C.UTF-8 locale...
-  #cat /usr/share/i18n/SUPPORTED >/etc/locale.gen
-  echo "C.UTF-8 UTF-8" >/etc/locale.gen
+    # Either enable all locales or the C.UTF-8 locale...
+    MAKEOPTS=-j
+    cat /usr/share/i18n/SUPPORTED >/etc/locale.gen
+    #echo "C.UTF-8 UTF-8" >/etc/locale.gen
 
-  # and generate those locales...
-  locale-gen
+    # and generate those locales...
+    locale-gen
 
-  # Set timezone to Etc/UTC...
-  #echo "Etc/UTC" >/etc/timezone
-  emerge --config sys-libs/timezone-data
+    # Set timezone to Etc/UTC...
+    #echo "Etc/UTC" >/etc/timezone
+    emerge --config sys-libs/timezone-data
 
-  # Update the system environment...
-  source /etc/profile
-  env-update
+    # Update the system environment...
+    source /etc/profile
+    env-update
 ZZEOF
       ;;
   esac
